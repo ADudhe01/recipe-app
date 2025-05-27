11baeaaf6cb40c311631e902df93120af7cc4898
ADudhe01 <atharva.dudhe@gmail.com>
2025-05-27 18:15:37 +1000

    Finished recipe app

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
+import styles from "./FoodDetails.module.css";
+import ItemList from "./ItemList";
 
 export default function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
   const API_KEY = "12bd6151686a4d8d9a8f5bdcad0a6ac5";
 
@@ -11,14 +14,55 @@ export default function FoodDetails({ foodId }) {
       const data = await response.json();
       console.log(data);
       setFood(data);
+      setIsLoading(false);
     }
     fetchFood();
   }, [foodId]);
   return (
     <div>
-      Food Details {foodId}
-      {food.title}
-      <img src={food.image} alt="" />
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+
+        <img className={styles.recipeImage} src={food.image} alt="" />
+        <div className={styles.recipeDetails}>
+          <span>
+            <strong>⏰ {food.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>🧑‍🧑‍🧒 Serves {food.servings}</strong>
+          </span>
+          <span>
+            <strong>
+              {food.vegetarian ? "🥦 Vegetarian" : "🍖 Non-Vegetarian"}
+            </strong>
+          </span>
+          <span>
+            <strong>{food.vegan ? "🐮 Vegan" : ""}</strong>
+          </span>
+        </div>
+
+        <div>
+          <strong>
+            <span>$ {(food.pricePerServing / 100).toFixed(3)} per serving</span>
+          </strong>
+        </div>
+
+        <h2>Ingredients</h2>
+        <ItemList food={food} isLoading={isLoading} />
+
+        <h2>Instructions</h2>
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading....</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
